544f9e04b68164ad017f0d736287810082d10961
akilsonredes <akilson.vasconcelos@gmail.com>
2025-03-25 13:10:47 +0000

    Tema3_Desafio_Mestre

--- xadrez.c
+++ xadrez.c
@@ -4,8 +4,7 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
-    // Nível Novato - Movimentação das Peças
+// Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
     // Implementação de Movimentação do Bispo
@@ -28,8 +27,27 @@ int main() {
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
+void  moverTorre (int casasT){
+    if (casasT > 0){
+    printf("Direita \n");
+    moverTorre (casasT - 1);
+}}
+void   moverBispo (int casasB){
+    if (casasB > 0){
+        printf ("Cima, Direita \n");
+        moverBispo (casasB - 1);
+        }}
+void    moverRainha (int casasR){
+    if (casasR > 0){
+        printf("Esquerda \n");
+        moverRainha (casasR -1);
+    }}
+
+int main() {
+    
+
 char EscolhaPeca;
-int i = 1;
+int i = 1, i2;
 
 printf ("***OPÇÕES DE PEÇAS DO XADREZ*** \n");
 printf ("Digite (T) para Torre \n");
@@ -42,34 +60,52 @@ scanf("%c",&EscolhaPeca);
 
 switch(EscolhaPeca){
     case 'T':
-    while (i <= 5) {
+        
+    moverTorre (5);
+   
+    /*while (i <= 5) {
         printf("Direita \n");
         i++;
-    }
+    }*/
+    
     break;
 
     case 'B':
-    do
+  
+    moverBispo (5);
+  
+    /*do
     {
         printf("Cima, Direita \n");
         i++;
-    } while (i <= 5);
+    } while (i <= 5);*/
+
     break;
 
     case 'R':
-    for (i; i <= 8; i++)
+    
+    moverRainha (8);
+
+    /*for (i; i <= 8; i++)
     {
         printf("Esquerda \n");
-    }
+    }*/
+    
     break;
 
     case 'C':
-    while (i <= 1){
+    
+    printf ("Digite o número de jogadas: ");
+    scanf ("%d", &i2);
+    printf("\n");
+
+    while (i <= i2){
         for (int j = 1; j <= 2; j++){
             printf("cima \n");
         }
         i++;
         printf("direita \n");
+        printf("\n");
     }
     break;
 }
